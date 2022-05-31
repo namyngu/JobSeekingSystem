b304802ceec18fd4e70654e8e429358213798d50
Gerard SD <gsam0006@student.monash.edu>
2022-05-31 16:26:04 +1000

    amending Administrator

--- JobSeekingSystem/src/Administrator.java
+++ JobSeekingSystem/src/Administrator.java
@@ -1,5 +1,9 @@
 public class Administrator extends User
 {
+
+    private boolean isPrimaryAdmin;
+
+    //default constructor needed
     public Administrator(int userID, String name, String userName, String password, boolean loggedIn, boolean active)
     {
         super(userID, name, userName, password, loggedIn, active);
