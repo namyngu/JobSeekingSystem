91237288168bb7718415ac55d80def72ba623295
Gerard SD <gsam0006@student.monash.edu>
2022-06-02 10:42:08 +1000

    adding control classes, amended entity classes to reflect class diagram

--- JobSeekingSystem/src/Administrator.java
+++ JobSeekingSystem/src/Administrator.java
@@ -3,7 +3,7 @@ public class Administrator extends User
 
     private boolean isPrimaryAdmin;
 
-    //default constructor needed
+
     public Administrator(int userID, String name, String userName, String password, boolean loggedIn, boolean active)
     {
         super(userID, name, userName, password, loggedIn, active);
---
+++ JobSeekingSystem/src/JSS.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class JSS
+{
+    private static ArrayList<String> categories;
+    private static int currentUserID;
+    private static int nextJobID;
+
+    public JSS()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        JSS obj = new JSS();
+    }
+
+}
