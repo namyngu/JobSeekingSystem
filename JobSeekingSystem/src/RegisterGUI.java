b065b3fa7d54ec8eeedde3d5815407e4a3263864
Gerard SD <gsam0006@student.monash.edu>
2022-06-04 08:49:14 +1000

    Radio Buttons on registration screen now set false when the other is true so both can not be selected

--- JobSeekingSystem/src/RegisterGUI.java
+++ JobSeekingSystem/src/RegisterGUI.java
@@ -10,6 +10,7 @@ TODO after registration return to login screen
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
 
 public class RegisterGUI
 {
@@ -62,6 +63,27 @@ public class RegisterGUI
                     System.out.println("Error - contact Admin with message: \n");
                     x.printStackTrace();
                 }
+            }
+        });
+        radioButtonJobseeker.addComponentListener(new ComponentAdapter()
+        {
+        });
+        radioButtonJobseeker.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                radioButtonRecruiter.setSelected(false);
+            }
+        });
+
+
+        radioButtonRecruiter.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                radioButtonJobseeker.setSelected(false);
             }
         });
     }
