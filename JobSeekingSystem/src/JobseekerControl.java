d5e5ecb064f7b22e082cf474ad0076826f7d7658
Brad Meyn <bradjmeyn@gmail.com>
2022-06-05 16:07:41 +1000

    update JS home to tabs

--- JobSeekingSystem/src/JobseekerControl.java
+++ JobSeekingSystem/src/JobseekerControl.java
@@ -1,3 +1,53 @@
+import java.util.ArrayList;
+
 public class JobseekerControl
 {
+
+    public void apply()
+    {
+
+    }
+
+    public void createSkill()
+    {
+
+    }
+
+    public void jobSearch()
+    {
+
+    }
+
+
+    public float matchScore(ArrayList<String> keywords, ArrayList skills)
+    {
+        float matchPercentage = 0;
+        return matchPercentage;
+    };
+
+    public void modifySkill()
+    {
+
+    }
+
+
+    public void viewApplications()
+    {
+
+    }
+
+    public void viewJobDetails(int jobId)
+    {
+
+    }
+
+    public void viewInvitations()
+    {
+
+    }
+
+
+
+
+
 }
