9d70abdba5441927b196570fd49c3ae15452ff3e
Tom <t.barker@uq.edu.au>
2022-06-06 13:30:52 +1000

    Quick update to  JSS.java so that importUserList now is called at Program Start again

--- JobSeekingSystem/src/JSS.java
+++ JobSeekingSystem/src/JSS.java
@@ -13,17 +13,15 @@ public class JSS
     private static int nextJobID;
     private final File_Control fileControl = new File_Control();
     private ArrayList<User> userList = new ArrayList<>();
-    private static Start StartControl;
 
-//    public JSS(Start myParent) {
-//        StartControl = myParent;
-//        //  JFrame frame = new JFrame("LoginGUI");
-//        //  frame.setContentPane(new LoginGUI().loginPanel);
-//        //  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        //  frame.pack();
-//        //  frame.setVisible(true);
-//        importUserList("JobSeekingSystem/users.csv");
-//    }
+    public JSS() {
+        //  JFrame frame = new JFrame("LoginGUI");
+        //  frame.setContentPane(new LoginGUI().loginPanel);
+        //  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //  frame.pack();
+        //  frame.setVisible(true);
+        importUserList("JobSeekingSystem/users.csv");
+    }
 
     //Verifies username/password & logs the user in.
     //TODO: can split this method into two. One for validation, one for logging in.
