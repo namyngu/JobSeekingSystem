07e9d845998b55c869cdca714760b88682919338
jclar0043 <jcla0043@student.monash.edu>
2022-06-07 02:30:03 +0930

    create job function update (halfway through)

--- JobSeekingSystem/src/CreateJobGUI.java
+++ JobSeekingSystem/src/CreateJobGUI.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.*;
 
 public class CreateJobGUI {
     private JPanel createJobPanel;
@@ -17,7 +21,7 @@ public class CreateJobGUI {
     private JTextField salaryText;
     private JComboBox salaryMenu;
     private JLabel skillsLabel;
-    private JComboBox comboBox1;
+    private JComboBox skillsMenu;
     private JList skillsList;
     private JButton addSkillButton;
     private JButton removeSkillButton;
@@ -42,4 +46,147 @@ public class CreateJobGUI {
     private JLabel statusLabel;
     private JComboBox statusMenu;
     private JLabel statusMessageLabel;
+    private JButton submitButton;
+
+    public CreateJobGUI() throws IOException {
+        JFrame frame1 = new JFrame("Create Job");
+        //frame1.setContentPane(this.createJobPanel);
+        //frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame1.pack();
+        //frame1.setVisible(true);
+
+        Job job = new Job();
+        populateSkills("JobSeekingSystem/src/Skills.csv");
+        populateKeywords("JobSeekingSystem/src/Keywords.csv");
+        //populateCategories("JobSeekingSystem/src/Categories.csv");
+
+        DefaultListModel skillsListGUI = new DefaultListModel();
+        ArrayList<String> skills = job.getSkills();
+        DefaultListModel keywordsListGUI = new DefaultListModel();
+        ArrayList<String> keywords = job.getKeywords();
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Submit button has been clicked");
+                job.setJobTitle(jobTitleText.getText());
+                System.out.println("jobTitle has been set to: " + job.getJobTitle());
+                job.setJobType(String.valueOf(jobTypeMenu.getSelectedItem()));
+                System.out.println("JobType has been set to: " + job.getJobType());
+                job.setSalary("$" + salaryText.getText() + " " + String.valueOf(salaryMenu.getSelectedItem()));
+                System.out.println("Salary has been set to: " + job.getSalary());
+
+                for (int i = 0; i < skillsList.getModel().getSize(); i++) {
+                    skills.add(String.valueOf(skillsList.getModel().getElementAt(i)));
+                }
+                job.setSkills(skills);
+                System.out.println("Skills have been set to: " + job.getSkills());
+
+                for (int i = 0; i < keywordsList.getModel().getSize(); i++) {
+                    keywords.add(String.valueOf(keywordsList.getModel().getElementAt(i)));
+                }
+                job.setKeywords(keywords);
+                System.out.println("Keywords have been set to: " + job.getKeywords());
+            }
+        });
+        addSkillButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                skillsListGUI.addElement(String.valueOf(skillsMenu.getSelectedItem()));
+                skillsList.setModel(skillsListGUI);
+            }
+        });
+        removeSkillButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                skillsListGUI.removeElement(skillsList.getSelectedValue());
+                skillsList.setModel(skillsListGUI);
+            }
+        });
+
+        addKeywordsButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                keywordsListGUI.addElement(String.valueOf(keywordsMenu.getSelectedItem()));
+                keywordsList.setModel(keywordsListGUI);
+            }
+        });
+        removeKeywordsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                keywordsListGUI.removeElement(keywordsList.getSelectedValue());
+                keywordsList.setModel(keywordsListGUI);
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) throws IOException {
+        CreateJobGUI objCreateJob = new CreateJobGUI();
+
+        JFrame frame = new JFrame("Create Job");
+        frame.setContentPane(objCreateJob.createJobPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        Job objJob = new Job();
+        objJob.setJobID(objCreateJob.generateJobID("JobSeekingSystem/src/Jobs.csv"));
+        objCreateJob.intJobIDLabel.setText(Integer.toString(objJob.getJobID()));
+        objCreateJob.intRecIDLabel.setText(Integer.toString(objJob.getRecruiterID()));  //will come from recruiter account details
+        objCreateJob.empNameLabel.setText(objJob.getEmployer());                        //will come from recruiter account details
+
+    }
+
+    public int generateJobID(String fileName) throws IOException {
+        FileReader file = new FileReader(fileName);
+        Scanner scan = new Scanner(file);
+        int numJobs = 1;
+
+        while (scan.hasNextLine()) {
+            System.out.println(scan.nextLine());
+            numJobs++;
+        }
+
+        file.close();
+        return numJobs;
+    }
+
+    public void populateCategories(String fileName) {
+
+    }
+
+    public void populateKeywords(String fileName) throws IOException {
+        FileReader file = new FileReader(fileName);
+        Scanner scan = new Scanner(file);
+        String returnString = "";
+
+        while (scan.hasNextLine()) {
+            returnString = scan.nextLine();
+            keywordsMenu.addItem(returnString);
+            //System.out.println(returnString + " added to keywords menu");
+        }
+
+        file.close();
+    }
+
+    public void populateSkills(String fileName) throws IOException {
+        FileReader file = new FileReader(fileName);
+        Scanner scan = new Scanner(file);
+        String returnString = "";
+
+        while (scan.hasNextLine()) {
+            returnString = scan.nextLine();
+            skillsMenu.addItem(returnString);
+            //System.out.println(returnString + " added to skills menu");
+        }
+
+        file.close();
+    }
+
 }
