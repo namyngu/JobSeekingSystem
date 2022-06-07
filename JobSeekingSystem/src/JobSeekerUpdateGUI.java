e925971e5d3721d8a15e2b3ec36a34c16b283586
Brad Meyn <bradjmeyn@gmail.com>
2022-06-07 22:42:52 +1000

    implement add skills lists to edit user profile

--- JobSeekingSystem/src/JobSeekerUpdateGUI.java
+++ JobSeekingSystem/src/JobSeekerUpdateGUI.java
@@ -1,21 +1,33 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class JobSeekerUpdateGUI {
     private JPanel updateSkillsPanel;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
-    private JList skillList;
+    private JList userSkillList;
     private JButton updateButton;
-    private JButton cancelButton;
     private JLabel clientName;
     private JLabel phoneLabel;
     private JLabel emailLabel;
     private JLabel addressLabel;
+    private JList allSkillList;
+    private JButton addSkillButton;
+    private JButton removeSkillButton;
+    private DefaultListModel userSkillsModel;
+    private DefaultListModel allSkillsModel;
+
 
     public JobSeekerUpdateGUI() {
-
+        userSkillsModel = new DefaultListModel<>();
+        allSkillsModel = buildModel("JobSeekingSystem/Skills.csv");
         JFrame frame = new JFrame("Job Title");
 
         frame.setContentPane(updateSkillsPanel);
@@ -24,39 +36,72 @@ public class JobSeekerUpdateGUI {
         frame.setResizable(true);
         frame.setVisible(true);
 
-        ArrayList<String> allSkills = new ArrayList<String>();
-        allSkills.add("Java");
-        allSkills.add("React");
-        allSkills.add("CSS");
-
+        ArrayList<String> allSkills = new ArrayList<String>();;
         ArrayList<String> mySkills = new ArrayList<String>();
 
-        mySkills.add("React");
 
+//        //build user skill list
+        buildList(mySkills, userSkillsModel, userSkillList);
 
-        buildSkillList(allSkills, mySkills);
+        //build all skills list
+        buildList(allSkills, allSkillsModel, allSkillList);
 
+        //action listener on button to add skill to user and remove from all skills list
+        addSkillButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userSkillsModel.addElement(allSkillList.getSelectedValue());
+
+                allSkillsModel.removeElement(allSkillList.getSelectedValue());
+            }
+        });
+
+        //action listener on button to move skill from user back to all skills list
+        removeSkillButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                allSkillsModel.addElement(userSkillList.getSelectedValue());
+                userSkillsModel.removeElement(userSkillList.getSelectedValue());
+
+            }
+        });
     }
 
+    //add data to JList using ListModel and ArrayList of data
+    private void buildList(ArrayList<String> list,DefaultListModel model, JList listGUI){
 
-    private void buildSkillList(ArrayList<String> allSkills, ArrayList<String> userSkills){
-
-        DefaultListModel model = new DefaultListModel();
-
-        //for each skill in allSkills create a checkbox
-        for (int i = 0; i < allSkills.size(); i++)
+        for (int i = 0; i < list.size(); i++)
         {
-            model.addElement(allSkills.get(i));
-            // Printing and display the elements in ArrayList
-            System.out.print(allSkills.get(i) + " ");
+            model.addElement(list.get(i));
         }
-
-        skillList.setModel(model);
-
+        listGUI.setModel(model);
     }
-        //loop through user skills
 
-        //if user skill === current skill check box
+    // take skills from csv file and return a model
+    public DefaultListModel buildModel(String fileName )  {
+            DefaultListModel model = new DefaultListModel<>();
+
+            try {
+                FileReader file = new FileReader(fileName);
+                Scanner scan = new Scanner(file);
+                String returnString = "";
+
+                while (scan.hasNextLine()) {
+                    returnString = scan.nextLine();
+                    model.addElement(returnString);
+                }
+
+                file.close();
+
+            } catch (FileNotFoundException e){
+                System.out.println("File not found");
+            } catch (IOException e){
+                //code
+            }
+
+        return model;
+        }
 
     }
 
