7aaaddcc1d115dd61e1c97a7c24b5f4da17b8c45
Gerard SD <gsam0006@student.monash.edu>
2022-06-07 13:35:41 +1000

    Username list now populates in AdminGUI

--- JobSeekingSystem/src/AdminGUI.java
+++ JobSeekingSystem/src/AdminGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.util.ArrayList;
 
 public class AdminGUI
@@ -20,6 +22,8 @@ public class AdminGUI
         this.adminControl = adminControl;
         this.program = program;
         this.userNames = new ArrayList<String>();
+        this.list = new DefaultListModel();
+        this.userList.setModel(this.list);
 
         JFrame frame = new JFrame("AdminHome");
         frame.setContentPane(this.adminHomePanel);
@@ -28,14 +32,18 @@ public class AdminGUI
         frame.setVisible(true);
 
         //requests num users from JSS and populates list that size of usernames only
+
         try{
             int numUsers = program.countUsers();
             for (int i=0; i<numUsers; i+=1)
             {
                 String name = this.program.retrieveUsername(i);
                 this.userNames.add(name);
+                this.refreshList(name);
 //                System.out.println("name added to list " + name);
             }
+
+//            this.userList.setVisible(true);
 
 
 
@@ -48,6 +56,28 @@ public class AdminGUI
             e.printStackTrace();
             PromptGUI error = new PromptGUI("Contact developer", e.toString());
         }
+        userList.addListSelectionListener(new ListSelectionListener()
+        {
+            @Override
+            public void valueChanged(ListSelectionEvent e)
+            {
+
+            }
+        });
+    }
+
+    private void refreshList(String name)
+    {
+        userList.setVisible(true);
+//        System.out.println("removing elements");
+//        list.removeAllElements();
+        System.out.println("adding name to list:" + name);
+        list.addElement(name);
+//        for (String each: userNames)
+//        {
+//
+//            list.addElement(each.toString());
+//        }
     }
 
 //    public AdminHome(AdminControl adminControl)
