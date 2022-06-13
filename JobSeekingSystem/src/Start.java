19f997335074917862606bf58a9277284cd1283c
Tom <t.barker@uq.edu.au>
2022-06-13 20:54:46 +1000

    Start - code quality pass as per conventions:
    /* NOTES ON CODE QUALITY:
     * Must include:
     * One Default Constructor - OK
     * Non Default Constructors - OK
     * Accessors for each field - OK
     * Mutators for each field - OK
     * A Display Method - OK
     * Methods must be sorted alphabetically, except constructors which always appear first. - OK
     */

--- JobSeekingSystem/src/Start.java
+++ JobSeekingSystem/src/Start.java
@@ -3,17 +3,52 @@ public class Start
     private JSS program;
     private LoginGUI startGUI;
 
+    // Default Constructor.
     public Start()
     {
         this.program = new JSS();
         this.startGUI = new LoginGUI(this.program);
-
     }
 
+    // Non-default Constructor.
+    public Start(JSS program, LoginGUI login)
+    {
+        this.program = program;
+        this.startGUI = login;
+    }
+
+    // Display method.
+    public void display()
+    {
+        System.out.println("Main JSS Program: " + program);
+        System.out.println("Login GUI: " + startGUI);
+    }
+
+    // Accessor methods.
+    public JSS getProgram()
+    {
+        return program;
+    }
+
+    public LoginGUI getStartGUI()
+    {
+        return startGUI;
+    }
+
+    // Method to begin main program execution.
     public static void main(String[] args)
     {
         Start obj = new Start();
+    }
 
-        //RecruiterHomeGUI recruiterHomeGUI = new RecruiterHomeGUI(new RecruiterControl());
+    // Mutator methods.
+    public void setProgram(JSS program)
+    {
+        this.program = program;
+    }
+
+    public void setStartGUI(LoginGUI login)
+    {
+        startGUI = login;
     }
 }
