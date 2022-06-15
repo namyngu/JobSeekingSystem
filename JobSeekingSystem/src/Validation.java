fa9eb6ed21daad081ae1e1b83d858d40fb1aa857
Tom <t.barker@uq.edu.au>
2022-06-15 21:24:35 +1000

    Validation - code documentation added.

--- JobSeekingSystem/src/Validation.java
+++ JobSeekingSystem/src/Validation.java
@@ -1,3 +1,9 @@
+/**
+ * This class contains methods used to validate various kinds of inputs
+ * against required parameters.
+ * @author  Team D - Tom Barker, Jakeob Clarke-Kennedy, Bradley Meyn, Hoang Nguyen, Gerard Samson-Dekker
+ */
+
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Timer;
@@ -5,6 +11,19 @@ import java.util.TimerTask;
 
 public class Validation {
 
+    /**
+     * This is the Default constructor.
+     */
+    public Validation(){
+
+    }
+
+    /**
+     * This method creates a warning label to be displayed when the user has
+     * entered invalid input.
+     * @param warningLabel  a JLabel onto which the warning will be displayed.
+     * @param message       a String contaning the warning message text.
+     */
     public static void invalidInputWarning(JLabel warningLabel, String message) {
         warningLabel.setText(message);
         warningLabel.setVisible(true);
@@ -17,11 +36,22 @@ public class Validation {
         }, 2000L); // 300 is the delay in millis
     }
 
+    /**
+     * This method checks to see if the input of a text field is empty or not.
+     * @param input a JTextField to be checked.
+     * @return      a Boolean describing if the JTextField is empty or not.
+     */
     public static boolean isInputBlank(JTextField input)
     {
         return input.getText().isEmpty();
     }
 
+    /**
+     * This method checks to see if a chosen username already exists in the system.
+     * @param users     an ArrayList of Users, containing all the Users in the system.
+     * @param username  a String contaning the desired username to be checked.
+     * @return          a Boolean describing if the chosen username already exists.
+     */
     public static boolean usernameExists(ArrayList<User> users, String username) {
         boolean exists = false;
         for (int i = 0; i < users.size(); i++) {
