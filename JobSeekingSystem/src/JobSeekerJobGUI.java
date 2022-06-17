0e5f4bdba4717d618457f6da02f7907375bde74b
Tom <t.barker@uq.edu.au>
2022-06-17 12:09:30 +1000

    Started on code documentation for JobSeekerJobGUI

--- JobSeekingSystem/src/JobSeekerJobGUI.java
+++ JobSeekingSystem/src/JobSeekerJobGUI.java
@@ -1,3 +1,8 @@
+/**
+ * This class is the GUI form for when a Jobseeker wants to view more details about a particular Job.
+ * @author  Team D - Tom Barker, Jakeob Clarke-Kennedy, Bradley Meyn, Hoang Nguyen, Gerard Samson-Dekker
+ */
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,6 +22,9 @@ public class JobSeekerJobGUI {
     private ArrayList<Job> jobList;
     private ArrayList<Location> locationList;
 
+    /**
+     * This is the Default constructor for the class.
+     */
     public JobSeekerJobGUI() {
 
         JFrame frame = new JFrame("Job Title");
@@ -29,6 +37,13 @@ public class JobSeekerJobGUI {
 
     }
 
+    /**
+     * This is the Non-default constructor for the class.
+     * @param control   a JobseekerControl Object which is controlling the currently
+     *                  logged-in Jobseeker.
+     * @param jobID     an Integer containing the ID number of the Job which the
+     *                  Jobseeker is wanting to view details for.
+     */
     public JobSeekerJobGUI(JobseekerControl control, int jobID)
     {
         JFrame frame = new JFrame("Job Title");
@@ -78,4 +93,7 @@ public class JobSeekerJobGUI {
             }
         });
     }
+
+    // TODO: Display method goes here.
+    // TODO: Setters and getters go here.
 }
