a908d33822a2628aa491bd38ecd81fb2a9b60db1
Tom Barker <tbar0017@student.monash.edu>
2022-06-19 09:21:25 +1000

    Added some doco for the PromptGUI class - more work needed.

--- JobSeekingSystem/src/PromptGUI.java
+++ JobSeekingSystem/src/PromptGUI.java
@@ -1,5 +1,6 @@
-/*
-Error GUI to display message to user
+/**
+ * This class is used to display a prompt to the user (such as an error message or instructions).
+ * @author  Team D - Tom Barker, Jakeob Clarke-Kennedy, Bradley Meyn, Hoang Nguyen, Gerard Samson-Dekker
  */
 
 import javax.swing.*;
@@ -17,14 +18,16 @@ public class PromptGUI
     private String message;
     private String details;
 
-//default constructor if no message sent.  TODO add appropriate message
+    /**
+     * This is the Default constructor for the class.
+     */
     public PromptGUI()
     {
         JFrame frame = new JFrame("USER PROMPT");
         frame.setContentPane(this.promptPanel);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.message = "";
-        this.details = "";
+        this.message = "Error!";
+        this.details = "Please contact an Administrator.";
         frame.pack();
         frame.setVisible(true);
         proceedButton.addActionListener(new ActionListener()
@@ -36,8 +39,12 @@ public class PromptGUI
             }
         });
     }
-//message only without stack trace to send to admin for situations the user should resolve
-    //eg blank text where input required
+
+    /**
+     * This is the Non-default constructor for the class.
+     * @param message a String containing the error or warning message to be
+     *                displayed on the prompt.
+     */
     public PromptGUI(String message)
     {
         JFrame frame = new JFrame("USER PROMPT");
@@ -59,8 +66,13 @@ public class PromptGUI
         });
     }
 
-    //detials added for admin to pass on to developer
-    // such as stack trace
+    /**
+     * This is a Non-default constructor for the class.
+     * @param message a String containing the error or warning message to be
+     *                displayed on the prompt.
+     * @param details a String containing error or warning message details to
+     *                be displayed on the prompt.
+     */
     public PromptGUI(String message, String details)
     {
         JFrame frame = new JFrame("USER PROMPT");
@@ -86,4 +98,16 @@ public class PromptGUI
             }
         });
     }
+
+    /**
+     * This is the display method for the class.
+     */
+    public void display() {
+        System.out.println("Prompt GUI: " + this);
+        System.out.println("Message: " + message);
+        System.out.println("Details: " + details);
+    }
+
+    // TODO: Setters and getters go here.
+
 }
